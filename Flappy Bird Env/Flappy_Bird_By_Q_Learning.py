7d6940db6ffb41a5d8eaac2243214582f428964b
Urlam Santosh Kumar <99114485+santoshurlam@users.noreply.github.com>
2023-10-06 11:21:46 +0530

    Update Flappy_Bird_By_Q_Learning.py

--- Flappy Bird Env/Flappy_Bird_By_Q_Learning.py
+++ Flappy Bird Env/Flappy_Bird_By_Q_Learning.py
@@ -108,10 +108,13 @@ test_graphs['test_score'] = np.array(test_scores)
 
 df1 = pd.DataFrame(graphs)
 df2 = pd.DataFrame(test_graphs)
+
 df1.set_index('Episode',inplace=True)
 df2.set_index('test_Episode',inplace=True)
+
 sma_period1 = 1000
 sma_period2 = 100
+
 df1['SMA1'] = df1['return'].rolling(window=sma_period1).mean()
 df1['SMA2'] = df1['reward'].rolling(window=sma_period1).mean()
 df1['SMA3'] = df1['score'].rolling(window=sma_period1).mean()
@@ -119,13 +122,15 @@ df1['SMA3'] = df1['score'].rolling(window=sma_period1).mean()
 df2['test_SMA1'] = df2['test_return'].rolling(window=sma_period2).mean()
 df2['test_SMA2'] = df2['test_reward'].rolling(window=sma_period2).mean()
 df2['test_SMA3'] = df2['test_score'].rolling(window=sma_period2).mean()
+
 df1.dropna(inplace=True)
 df2.dropna(inplace=True)
+
 # SMA1 = list(df1['SMA1'])
 # SMA2 = list(df1['SMA2'])
 # SMA3 = list(df1['SMA3'])
 
-'''Graphs for the trained episodes'''
+''' Graphs for the trained episodes'''
 
 '''Creating Subplots'''
 
@@ -154,7 +159,7 @@ axes1[2].legend(loc = 0)
 plt.grid()
 plt.show()
 
-'''Graphs for test episodes'''
+''' Graphs for test episodes'''
 fig2,axes2 = plt.subplots(1,3,figsize = (14,7))
 fig2.suptitle("FlappyBird-v0 using Q-Learning Algorithm for testing",fontweight = 'bold',color = 'k')
 
